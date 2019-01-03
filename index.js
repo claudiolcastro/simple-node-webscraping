a51b8e069a399f87a7b490947bae6f09ae3d0aa5
Claudio Castro <claudioluiz.castro@gmail.com>
2019-01-03 17:24:31 -0200

    Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Ignore Favicon request
 app.use((request, response, next) => {
   if (request.url === 'favicon.ico') {
